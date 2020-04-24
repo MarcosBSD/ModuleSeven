f4485effa12a956fad6fe6672272186fc0a475a4
MarcosBSD <mabarrera@csumb.edu>
2020-04-23 17:13:49 -0700

    Still working on it

--- app/src/main/java/com/example/moduleseven/MainActivity.java
+++ app/src/main/java/com/example/moduleseven/MainActivity.java
@@ -1,72 +1,66 @@
 package com.example.moduleseven;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity
 {
-   Map<Course, Integer> courseMap = new HashMap<Course, Integer>();;
-   private Boolean classMapCreated = false;
    @Override
    protected void onCreate(Bundle savedInstanceState)
    {
-      courseMap = initialize(courseMap);
       super.onCreate(savedInstanceState);
       setContentView(R.layout.activity_main);
+      ListView courseListView = (ListView)findViewById(R.id.lvCourses);
+
+      Course CST300 = new Course(
+            300,
+            getString(R.string.name300),
+            getString(R.string.desc300));
+
+      Course CST338 = new Course(
+            338,
+            getString(R.string.name338),
+            getString(R.string.desc338));
+
+      ArrayList<Course> courseList = new ArrayList<>();
+      courseList.add(CST300);
+      courseList.add(CST338);
+
+      SimpleAdapter courseAdapter = new SimpleAdapter(this,courseList,android.R.layout.simple_list_item_2,
    }
 
-   Map<Course, Integer> initialize(Map<Course,Integer> courseMap) {
-      if (classMapCreated == true) {
-         return courseMap;
-      }
-      else {
-         courseMap.put(new Course("CST300","Major Pro Seminar",
-               "Helps students identify and articulate personal, " +
-                     "professional, and social goals. Provides an integrated " +
-                     "overview of the computer science and communication " +
-                     "design majors and their requirements. Students develop" +
-                     " a plan for their learning goals. Students learn " +
-                     "writing, presentation, research and critical-thinking " +
-                     "skills within the diversified fields of information " +
-                     "technology and communication design. Students learn " +
-                     "how to analyze, predict, and articulate trends in the " +
-                     "academic, public service,"), 300);
-         courseMap.put(new Course("CST338","Software Engineering",
-               "Provides students with the fundamental concepts to " +
-                     "develop large-scale software, focusing on the " +
-                     "object-oriented programming techniques. Coverage " +
-                     "includes the introduction to Java programming language," +
-                     " object-oriented programming, software life cycle and " +
-                     "development processes, requirements analysis, and " +
-                     "graphical user interface development."), 338);
-         return courseMap;
-      }
-   }
 }
 
 class Course {
-   private String id;
+   private int id;
    private String name;
    private String description;
 
 
-   public Course(String id, String name, String description)
+   public Course(int id, String name, String description)
    {
       this.id = id;
       this.name = name;
       this.description = description;
    }
 
-   public String getId()
+   public int getId()
    {
       return id;
    }
 
-   public void setId(String id)
+   public void setId(int id)
    {
       this.id = id;
    }
